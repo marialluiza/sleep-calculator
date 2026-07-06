7c4ea435c1a24bec3eb20cb4f86590fefe432827
marialluiza <malululicha@gmail.com>
2026-07-06 15:25:06 -0300

    feat: implementa titulo principal

---
+++ src/components/sections/home/IntroTitle.tsx
@@ -0,0 +1,20 @@
+const IntroTitle = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <p className="text-primary font-mono">CALCULADORA DE JANELAS</p>
+
+      <h1 className="font-title text-5xl leading-tight">
+        Acorde <span className="text-primary italic">dencansado</span>,
+        <br />
+        não apenas acordado.
+      </h1>
+      <p className="text-muted-foreground">
+        O segredo não é dormir mais — é acordar no momento certo do seu ciclo.
+        <br />
+        Calcule suas janelas ideais de sono em segundos.
+      </p>
+    </div>
+  );
+};
+
+export default IntroTitle;
