f1430f254e8ee2dcc319ed30b83837f8aea97aa4
marialluiza <malululicha@gmail.com>
2026-07-06 12:14:12 -0300

    feat: implementa header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,18 +2,20 @@ import { Moon } from "lucide-react";
 
 const HeaderComponent = () => {
   return (
-    <div className="w-full flex justify-between p-8  items-center">
-      <div className="w-full flex gap-4  items-center">
-        <Moon size={18} className="text-primary" strokeWidth={1.5} />
-        <h1>CiCLOS DO SONO</h1>
+    <header className="font-mono">
+      <div className="w-full flex p-6 tracking-wider text-muted-foreground">
+        <div className="w-full flex gap-3 items-center">
+          <Moon size={18} className="text-primary" strokeWidth={1.5} />
+          <h1>CICLOS DO SONO</h1>
+        </div>
+
+        <div className="w-full flex justify-end items-center text-xs">
+          <p>90 min/ ciclo</p>
+        </div>
       </div>
 
-      <div className="w-full flex justify-end ">
-        <p>90 min/ ciclo</p>
-      </div>
-
-      <div className="divide-y-4 divide-yellow-600" />
-    </div>
+      <div className="w-full h-px bg-border" />
+    </header>
   );
 };
 
